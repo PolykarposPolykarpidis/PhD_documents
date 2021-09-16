631f6016ff933ea0fd674eea6c0db4c2d7e1202e
PolykarposPolykarpidis <61109950+PolykarposPolykarpidis@users.noreply.github.com>
2021-09-16 12:17:03 +0300

    Add files via upload

---
+++ entropy_variable_with_2_values.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+def a_term_of_entropy(p):
+	if p == 0:
+		return 0
+	return p * math.log2(p) * (-1)
+
+test = list(np.arange(0.0, 1.01, 0.01))
+pE = [a_term_of_entropy(x)+a_term_of_entropy(1-x) for x in test]
+
+plt.plot(test, pE)
+plt.title('Εντροπία - Πιθανότητα')
+plt.ylabel('H(X)')
+plt.xlabel('p(X=1)')
+plt.tight_layout()
+plt.savefig('entropy.png', bbox_inches='tight', dpi=600)
+plt.show()
---
+++ entropy_variable_with_3_values.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from itertools import product
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from mpl_toolkits import mplot3d
+import matplotlib.animation as animation
+
+
+def a_term_of_entropy(p):
+	if p == 0:
+		return 0
+	return p * math.log2(p) * (-1)
+
+
+test = np.arange(0.0, 1.01, 0.01)
+print(len(test))
+product_test = list(product(test, test, test))
+acceptable_triad = []
+
+for i in product_test:
+	if sum(i) == 1:
+		acceptable_triad.append(i)
+		
+x = []
+y = []
+z = []
+
+for i in acceptable_triad:
+	x.append(i[0])
+	y.append(i[1])
+	z.append(a_term_of_entropy(i[0])+a_term_of_entropy(i[1])+a_term_of_entropy(i[2]))
+
+ax = plt.axes(projection="3d")
+ax.plot3D(x,y,z, label='Εντροπία - Πιθανότητα')
+ax.set_xlabel('x = 1')
+ax.set_ylabel('x = 2')
+ax.set_zlabel('H(X)', rotation = 0)
+plt.show()
+
