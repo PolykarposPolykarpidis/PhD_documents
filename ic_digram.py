ebedbee0d49e9835a61f3c0680812b3fbc12d630
PolykarposPolykarpidis <61109950+PolykarposPolykarpidis@users.noreply.github.com>
2021-09-24 13:10:05 +0300

    Add files via upload

--- ic_digram.py
+++ ic_digram.py
@@ -12,7 +12,7 @@ import math
 infinity = 10 #we consider as 10 the infinity
 
 def ic(p):
-	#We know that when p(X=x) = 0 then IC value is + infinity. But here we are putting a 'large' constant value to represent this
+	#We know that when p(X=x) = 0 then IC value is + infinity. But here we are putting a 'large' constant value to represent the infiity and we show only a part of y to give the sence of infinity
 	if p == 0:
 		return infinity
 	return math.log2(p) * (-1)
@@ -25,5 +25,9 @@ plt.title('IC - πιθανότητα')
 plt.ylabel('IC')
 plt.xlabel('p(X=x)')
 plt.tight_layout()
+plt.ylim([0.0, 8.0]) #we show only a part of y to give the sence of infinity
+
+
+
 plt.savefig('ic.png', bbox_inches='tight', dpi=600)
 plt.show()
